0170432d32f8ee9a944449d031a8af3a2e0ef9e5
Patrick S <sopatrick@outlook.com>
2023-09-07 20:56:48 +1000

    excercises 1

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -3,13 +3,13 @@
 // Execute `rustlings hint move_semantics1` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
-#[test]
 fn main() {
     let vec0 = vec![22, 44, 66];
 
-    let vec1 = fill_vec(vec0);
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+
+    let mut vec1 = fill_vec(vec0);
+
 
     assert_eq!(vec1, vec![22, 44, 66, 88]);
 }
