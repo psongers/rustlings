dfa5108e9f3fb9dea5b0a349bb52c36e2df3505d
Patrick Song <sopatrick@outlook.com>
2023-11-01 02:03:51 +1100

    some more solutions

--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -5,11 +5,14 @@
 // Execute `rustlings hint strings2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
-    if is_a_color_word(word) {
+
+    // because String implements deref<target=str>, it can be dereferenced (*word) as a str
+    // with deref coercion this allows for the syntax below (compiler auto derefs as needed)
+    // it will do &(*word), where (*word) becomes type str
+    // https://stackoverflow.com/questions/23975391/how-to-convert-a-string-into-a-static-str
+    if is_a_color_word(&word) {
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
